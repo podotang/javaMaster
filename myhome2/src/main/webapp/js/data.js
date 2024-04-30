fd8de852e525c490b961d28cc6c358f2abe0af53
admin <admin@DESKTOP-AULLVSO>
2024-04-30 09:12:18 +0900

    이거사용

---
+++ myhome2/src/main/webapp/js/data.js
@@ -0,0 +1,19 @@
+
+
+const members = [
+	{num : 1001, name: '정유진', point: 90},
+	{num : 1002, name: '서유진', point : 70},
+	{num : 1003, name: '오유진', point : 99}
+];
+
+// 배열 for
+// for(let mem of members){}
+
+members.forEach(function(item,idx,ary){
+	if(item.point>95){
+	console.log(item,idx,ary);
+	}
+	
+});
+
+
